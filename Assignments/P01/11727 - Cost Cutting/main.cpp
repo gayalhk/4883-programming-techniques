654d731c47934a38db13d8eb378a7f4a9add8acc
gayalhk <35520396+gayalhk@users.noreply.github.com>
2020-10-09 00:57:46 -0500

    Create main.cpp

---
+++ Assignments/P01/11727 - Cost Cutting/main.cpp
@@ -0,0 +1,45 @@
+//////////////////////////////////////////////////////////////////////////////////
+//
+// Author:           Gayal Hewakuruppu
+// Email:            gayal.hewakuruppu07@gmail.com
+// Course:           CMPS 4883 Programming Techniques
+// Proffessor:       Dr Terry Griffin
+//
+// Description:      This program takes 3 people's salaries and determines the  
+//                   middle vaue of the 3 in order to decide which employee is 
+//                   not to be layed off.
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+  ifstream infile;
+  infile.open("input.txt");
+
+  int T;
+  int Salaries[3];
+
+  infile >> T;
+
+  for(int i = 0; i < T; i++){
+    infile >> Salaries[0] >> Salaries[1] >> Salaries[2];
+    for(int a = 0; a < 2; a++)
+    {
+      for(int b = 0; b < 2 - a; b++)
+      {
+        if(Salaries[b] > Salaries[b+1])
+        {
+          int temp = Salaries[b];
+          Salaries[b] = Salaries[b+1];
+          Salaries[b+1] = temp;
+        }
+      }
+    }
+    cout << "Case " << i+1 << ": " << Salaries[1] << endl;
+  }
+  infile.close();
+}
