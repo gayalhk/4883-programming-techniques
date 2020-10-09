9591bab0b5299b5f43f5ac52051440d635945419
gayalhk <35520396+gayalhk@users.noreply.github.com>
2020-10-09 00:59:41 -0500

    Create main.cpp

---
+++ Assignments/P01/12372 - Packing for Holiday/main.cpp
@@ -0,0 +1,40 @@
+//////////////////////////////////////////////////////////////////////////////////
+//
+// Author:           Gayal Hewakuruppu
+// Email:            gayal.hewakuruppu07@gmail.com
+// Course:           CMPS 4883 Programming Techniques
+// Proffessor:       Dr Terry Griffin
+//
+// Description:      This program takes 3 values (legth,width and height of a box)   
+//                    and checks if it fits a 20x20x20 suitcase.
+//                   
+//
+/////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+  ifstream infile;
+  infile.open("input.txt");
+
+  int T;
+  int length,width,height;
+
+  infile >> T;
+
+  for(int i = 0; i < T; i++){
+    infile >> length >> width >> height;
+
+    if((length<=20) && (width<=20) && (height<=20)){
+      cout << "Case " << i+1 << ": good" << endl;
+    }
+    
+    else{
+      cout << "Case " << i+1 << ": bad" << endl;
+    }
+  }
+  infile.close();
+}
